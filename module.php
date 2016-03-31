c704be71f0c7bf31cabbc63ef75c23bbbf6c7865
Ralfs <lagzdar@uni.coventry.ac.uk>
2016-03-31 07:12:25 +0000

    Database connection

---
+++ module.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../favicon.ico">
+
+    <title>Module information</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body style="padding-top:50px; padding-bottom:60px;">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">Project</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    <div class="container">
+        <h1>Module information</h1>
+        <div class="col-xs-8">
+            <div class="panel panel-primary">
+              <div class="panel-heading">Java</div>
+              <div class="panel-body" style="padding:0">
+                  <table class = "table table-bordered" style="margin-bottom:0">
+   
+   <thead>
+      <tr>
+         <th>Teacher</th>
+         <th>Classroom</th>
+         <th>Time</th>
+      </tr>
+   </thead>
+   
+   <tbody>
+      <tr>
+         <td>Kevin Gong</td>
+         <td>A202</td>
+         <td>Monday 12:00</td>
+      </tr>
+      
+      <tr>
+         <td>Jason Lee</td>
+         <td>C307</td>
+         <td>Tuesday 14:00</td>
+      </tr>
+      
+      <tr>
+         <td>Cindy Chan</td>
+         <td>G408</td>
+         <td>Thursday 12:00</td>
+      </tr>
+   </tbody>
+	
+</table>
+              </div>
+            </div>
+        </div>
+        <div class="col-xs-4">
+            
+            <div class="panel panel-info">
+              <div class="panel-heading">Major notifications</div>
+              <div class="panel-body" style="padding:0">
+                <ul class = "list-group" style="margin-bottom:0">
+
+               <li class = "list-group-item">
+                  <span class = "badge">2016/03/30</span>
+                  Mathematics is cancelled tommorow
+               </li>
+               <li class = "list-group-item">
+                  <span class = "badge">2016/03/27</span>
+                  Sign up for basketball this wednesday
+               </li>
+
+            </ul>
+              </div>
+            </div>
+        </div>
+    </div>
+      
+    <div id="footer" class="container">
+        <nav class="navbar navbar-default navbar-fixed-bottom">
+            <div class="navbar-inner navbar-content-center">
+                <p class="text-muted credit" style="padding: 10px; margin-top:10px;">
+                    Coventry University and Guangdong University
+                </p>
+            </div>
+        </nav>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
